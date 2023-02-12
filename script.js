1686b637d93bf763be7eb3a3fb625834dd3ef7bf
Manojparthasarathy <121498237+Manojparthasarathy@users.noreply.github.com>
2023-02-12 03:01:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var arr =[{
+    "name":"P.Manoj",
+    "Age": "30",
+    "gender": "male",
+    "qulification": ["Diploma in civil engineering","Bacholer of civil engineering"],
+    "experience": "7 years",
+    "company_name": "Tamilnadu PWD",
+    "location": "Tiruvannamalai",
+    "Languages": ["tamil","english","kannada","hindi"],
+    "Phone": "8668176728",
+
+}]
+console.log(arr);
